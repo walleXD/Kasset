02af17817a7bb06a15c873b5272f6257fbaf89b5
Walee Ahmed <walee.tausif@gmail.com>
2017-09-18 17:35:29 -0400

    Inital commit w/ basic project structure 🎉

---
+++ src/main/index.js
@@ -0,0 +1,84 @@
+import { app, BrowserWindow } from 'electron'
+import windowStateKeeper from 'electron-window-state'
+
+const isDev = process.env.NODE_ENV !== 'production'
+
+let mainWindow
+let storybookWindow // eslint-disable-line no-unused-vars
+
+const createMainWindow = async () => {
+  const mainWindowState = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 800
+  })
+
+  const win = new BrowserWindow({
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height
+  })
+
+  mainWindowState.manage(win)
+
+  const url = isDev
+    ? 'http://localhost:9080'
+    : `file://${__dirname}/index.html`
+
+  if (isDev) win.webContents.openDevTools()
+
+  win.loadURL(url)
+
+  win.on('closed', () => {
+    mainWindow = null
+  })
+
+  return win
+}
+
+const createStorybookWindow = () => {
+  const storybookWindowState = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 800,
+    file: 'storybook-window-state.json',
+    path: app.getPath('userData')
+  })
+
+  const win = new BrowserWindow({
+    x: storybookWindowState.x,
+    y: storybookWindowState.y,
+    width: storybookWindowState.width,
+    height: storybookWindowState.height
+  })
+
+  storybookWindowState.manage(win)
+
+  const url = 'http://localhost:9009'
+
+  win.loadURL(url)
+
+  win.on('closed', () => {
+    storybookWindow = null
+  })
+
+  return win
+}
+
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications to stay open
+  // until the user explicitly quits
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  // On macOS it is common to re-create a window
+  // even after all windows have been closed
+  if (mainWindow === null) mainWindow = createMainWindow()
+  if (storybookWindow === null) storybookWindow = createStorybookWindow()
+})
+
+// Create main BrowserWindow when electron is ready
+app.on('ready', () => {
+  if (isDev) storybookWindow = createStorybookWindow()
+  mainWindow = createMainWindow()
+})
