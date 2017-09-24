e7b20b94100be049343173a53e7a4394f2c00410
Walee Ahmed <walee.tausif@gmail.com>
2017-09-24 15:36:53 -0400

    DB refactored to use sqlite with RxDB

--- src/main/lib/db.js
+++ src/main/lib/db.js
@@ -3,12 +3,17 @@ import RxDB from 'rxdb'
 let db = null
 
 export const initDb = async (dir = '/tmp/db') => {
-  RxDB.plugin(require('pouchdb-adapter-leveldb'))
-  RxDB.plugin(require('pouchdb-quick-search'))
-  db = await RxDB.create({
-    name: dir,
-    adapter: 'leveldb'
-  })
+  try {
+    RxDB.plugin(require('pouchdb-adapter-node-websql'))
+    RxDB.plugin(require('pouchdb-quick-search'))
+    db = await RxDB
+      .create({
+        name: '/tmp/kasset',
+        adapter: 'websql'
+      })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export default db
